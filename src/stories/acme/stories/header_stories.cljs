c95645505ea1e81d332d799ea6ba03dc08106d82
shaolang <shaolang@ymail.com>
2021-02-14 07:08:07 +0800

    Create header_stories

---
+++ src/stories/acme/stories/header_stories.cljs
@@ -0,0 +1,10 @@
+(ns acme.stories.header-stories
+  (:require [acme.components.header :refer [header]]
+            [reagent.core :as r]))
+
+(def ^:export default
+  #js {:title     "Header Component"
+       :component (r/reactify-component header)})
+
+(defn ^:export HelloWorldHeader []
+  (r/as-element [header "Hello, World!"]))
